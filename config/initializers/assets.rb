12768a9f9e91eff9e6ed51d1de7f452a0b935d14
Haile <hshavers@gmail.com>
2015-10-24 02:07:58 -0700

    updated home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+
+
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
