1deea4a2cdaef02966cbfba25a14c6aef8a29318
Haile <hshavers@gmail.com>
2015-10-24 17:02:42 -0700

    Final Push

--- send_sms.rb
+++ send_sms.rb
@@ -7,7 +7,7 @@ auth_token = 'c77dcbbad59ca5fb4ea10e86fd5fe05f'
 @client = Twilio::REST::Client.new account_sid, auth_token
  
 message = @client.account.messages.create(:body => "Hotline Bling",
-	:media_url => 'https://www.youtube.com/watch?v=V86BRbdVqvM',
+	# :media_url => 'https://www.youtube.com/watch?v=V86BRbdVqvM',
 
     :to => "+16197291100",     # Replace with your phone number
     :from => "+16507275107")   # Replace with your Twilio number
